681284f978aefee621a559070092b95b24d06205
Guillermo <guillef33@gmail.com>
2025-05-02 18:14:40 -0300

    refactor logic

--- js/script.js
+++ js/script.js
@@ -1,122 +1,135 @@
-const playerOne = document.querySelector("#playerOne");
-const playerTwo = document.querySelector("#playerTwo");
 const ball = document.querySelector("#ball");
 const court = document.querySelector("#court");
 const marker = document.querySelector("#clickMarker");
-const turnoPlayerOne = document.querySelector("#turnoPlayerOne");
-const turnoPlayerTwo = document.querySelector("#turnoPlayerTwo");
-const tiroPlayerOne = document.querySelector("#tiroPlayerOne");
-const tiroPlayerTwo = document.querySelector("#tiroPlayerTwo");
-const resultadoJugadorUno = document.querySelector("#resultadoJugadorUno");
-const resultadoJugadorDos = document.querySelector("#resultadoJugadorDos");
 const ladoArriba = document.querySelector("#ladoArriba");
 const ladoAbajo = document.querySelector("#ladoAbajo");
 
+const jugadorUno = {
+  node: document.querySelector("#playerOne"),
+  turno: document.querySelector("#turnoPlayerOne"),
+  velocidad: 1,
+  precisionDeTiro: 1,
+  puntaje: document.querySelector("#resultadoJugadorUno"),
+  puntos: 0,
+  mensajeTiro: document.querySelector("#tiroPlayerOne"),
+};
+
+const jugadorDos = {
+  node: document.querySelector("#playerTwo"),
+  turno: document.querySelector("#turnoPlayerTwo"),
+  velocidad: 1,
+  precisionDeTiro: 1,
+  puntaje: document.querySelector("#resultadoJugadorDos"),
+  puntos: 0,
+  mensajeTiro: document.querySelector("#tiroPlayerTwo"),
+};
+
 let turno = 0;
-let puntosJugadorUno = 0;
-let puntosJugadorDos = 0;
 
-court.addEventListener("click", handleCourtClick);
+court.addEventListener("click", manejarClickEnLaCancha);
 
+function manejarClickEnLaCancha(e) {
+  let coordenadasTiro = calcularCoordenadasDelClick(e);
+  dibujarMarker(coordenadasTiro.x, coordenadasTiro.y);
+  animarPelota(coordenadasTiro.x, coordenadasTiro.y);
+  const zona = evaluarZonaTiro(coordenadasTiro.x, coordenadasTiro.y);
+  turnos(coordenadasTiro.x, coordenadasTiro.y, zona);
+}
 
-function ladoQueFueClickeado (x, y) {
-  const arribaRect = ladoArriba.getBoundingClientRect();
-  const abajoRect = ladoAbajo.getBoundingClientRect();
+function calcularCoordenadasDelClick(e) {
+  return {
+    x: e.clientX,
+    y: e.clientY,
+  };
+}
 
-  const estaEnLadoArriba =
+function dibujarMarker(x, y) {
+  marker.style.opacity = 1;
+  marker.style.left = x + "px";
+  marker.style.top = y + "px";
+}
+
+function animarPelota(x, y) {
+  gsap.to(ball, {
+    top: y + "px",
+    left: x + "px",
+    duration: 1,
+    ease: "linear",
+  });
+}
+
+function evaluarZonaTiro(x, y) {
+  const arribaRect = document
+    .querySelector("#ladoArriba")
+    .getBoundingClientRect();
+  const abajoRect = document
+    .querySelector("#ladoAbajo")
+    .getBoundingClientRect();
+
+  const enArriba =
     x >= arribaRect.left &&
     x <= arribaRect.right &&
     y >= arribaRect.top &&
     y <= arribaRect.bottom;
 
-  const estaEnLadoAbajo =
+  const enAbajo =
     x >= abajoRect.left &&
     x <= abajoRect.right &&
     y >= abajoRect.top &&
     y <= abajoRect.bottom;
 
-  if (estaEnLadoArriba) {
-    console.log("Clic en ladoArriba");
+  if (enArriba) return "arriba";
+  if (enAbajo) return "abajo";
+  return "fuera";
+}
 
-    procesarTurno(estaEnLadoArriba, x, y );
-  } else if (estaEnLadoAbajo) {
-    console.log("Clic en ladoAbajo");
-    procesarTurno(estaEnLadoAbajo, x, y );
+function manejarTurno(jugadorActivo, jugadorPasivo, zona, x, y) {
+  jugadorActivo.turno.style.opacity = 1;
+  jugadorPasivo.turno.style.opacity = 0;
+  jugadorPasivo.mensajeTiro.style.opacity = 0; // Reset mensaje de tiro del jugador pasivo
+
+  if (zona === "arriba" && jugadorActivo === jugadorUno) {
+    // Buen tiro de jugadorUno
+    mostrarMensajeDeTiro(jugadorUno, "¡Buen tiro jugador Uno!");
+    movePlayerToTheBall(y, x, jugadorUno.node);
+    jugadorPasivo.turno.style.opacity = 1;
+  } else if (zona === "abajo" && jugadorActivo === jugadorDos) {
+    // Buen tiro de jugadorDos
+    mostrarMensajeDeTiro(jugadorDos, "¡Buen tiro jugador Dos!");
+    movePlayerToTheBall(y, x, jugadorDos.node);
+    jugadorPasivo.turno.style.opacity = 1;
   } else {
-    console.log("Clic fuera de los lados");
+    // Fallo
+    mostrarMensajeDeTiro(
+      jugadorActivo,
+      `Has fallado el tiro ${
+        jugadorActivo === jugadorUno ? "jugador Uno" : "jugador Dos"
+      }`
+    );
+    // Sumar puntos al jugador pasivo si falló el tiro
+    sumarPuntos(jugadorPasivo);
+    jugadorPasivo.turno.style.opacity = 1;
   }
 }
 
-
-function handleCourtClick(e) {
-  const x = e.clientX;
-  const y = e.clientY;
-
-  ladoQueFueClickeado(x, y);
-
-  marker.style.opacity = 1;
-  marker.style.left = x + "px";
-  marker.style.top = y + "px";
-
-  gsap.to(ball, {
-    top: marker.style.top,
-    left: marker.style.left,
-    duration: 1,
-    ease: "linear",
-  });
-
-}
-
-function sumarPuntos () {
+function turnos(x, y, zona) {
   if (turno % 2 === 0) {
-    puntosJugadorDos++;
-    resultadoJugadorDos.textContent = puntosJugadorUno;
-    turnoPlayerTwo.style.opacity = 1;
-    turnoPlayerTwo.textContent = "El jugador dos ha ganado, porque erraste"
+    manejarTurno(jugadorUno, jugadorDos, zona, x, y);
   } else {
-    puntosJugadorDos++;
-    resultadoJugadorUno.textContent = puntosJugadorUno;
-    turnoPlayerOne.style.opacity = 1;
-    turnoPlayerOne.textContent = "El jugador uno ha ganado, porque erraste"
-
+    manejarTurno(jugadorDos, jugadorUno, zona, x, y);
   }
 
-  
+  turno++; 
 }
 
+function mostrarMensajeDeTiro(jugador, mensaje) {
+  jugador.mensajeTiro.textContent = mensaje;
+  jugador.mensajeTiro.style.opacity = 1;
+}
 
-
-function procesarTurno(lado, x, y) {
-  if (turno % 2 === 0) {
-    turnoPlayerOne.style.opacity = 0;
-    turnoPlayerTwo.style.opacity = 1;
-
-    movePlayerToTheBall(y, x, playerOne);
-
-    if (lado) {
-      mostrarMensaje(tiroPlayerOne, "Buen tiro");
-    } else {
-      mostrarMensaje(tiroPlayerOne, "Has fallado, tiraste fuera de pista");
-      puntosJugadorDos++;
-      resultadoJugadorDos.textContent = puntosJugadorDos;
-    }
-
-  } else {
-    // Turno Jugador 2
-    turnoPlayerOne.style.opacity = 1;
-    turnoPlayerTwo.style.opacity = 0;
-
-    movePlayerToTheBall(y, x, playerTwo);
-
-    if (lado) {
-      mostrarMensaje(tiroPlayerTwo, "Buen tiro");
-    } else {
-      mostrarMensaje(tiroPlayerTwo, "Has fallado, tiraste fuera de pista");
-    
-    }
-  }
-
-  turno++;
+function sumarPuntos(jugador) {
+  jugador.puntos = jugador.puntos + 1;
+  jugador.puntaje.textContent = jugador.puntos;
 }
 
 function movePlayerToTheBall(x, y, player) {
@@ -128,34 +141,6 @@ function movePlayerToTheBall(x, y, player) {
   });
 }
 
-function mostrarMensaje(elemento, mensaje) {
-  elemento.textContent = mensaje;
-  elemento.style.opacity = 1;
-  setTimeout(() => {
-    elemento.style.opacity = 0;
-  }, 1000);
+function reiniciarElPunto () {
+  
 }
-
-function playerCanReach() {}
-
-function theBallCrossTheNet() {}
-
-function theBallIsIn() {}
-
-// playerTwo.addEventListener("click", () => {
-//   const animateBall = gsap.to(ball, {
-//     top: "70px",
-//     left: "100px",
-//     duration: 1,
-//     ease: "linear",
-//   });
-// });
-
-// playerOne.addEventListener("click", () => {
-//   const animateBall = gsap.to(ball, {
-//     top: "420px",
-//     left: "240px",
-//     duration: 1,
-//     ease: "linear",
-//   });
-// });
